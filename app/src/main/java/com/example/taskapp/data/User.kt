6993ef7092939efdb2534672043c70dc220f64a4
Menusha Thathsara <161927072+Menusha2k1@users.noreply.github.com>
2024-05-12 00:48:41 +0530

    Implement User.kt

---
+++ app/src/main/java/com/example/taskapp/data/User.kt
@@ -0,0 +1,12 @@
+package com.example.taskapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "user Table")
+class User (
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val firstname:String,
+    val lastName:String,
+    val age:Int
+)
